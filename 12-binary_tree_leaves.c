1cf8f778cfa360a27ab7a25ea5dd4ab22ed82ad6
zaynOm <ouaziz0626@gmail.com>
2023-12-01 00:30:53 +0100

    task 12: Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -14,8 +14,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (1);
 
-	binary_tree_leaves(tree->left);
-	binary_tree_leaves(tree->right);
-
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
